127da00b7489b349061baefc60ecde85e65b5133
Szymon Duchniewicz <szduchniewicz@gmail.com>
2022-01-25 12:04:02 +0000

    Add skeleton code for Actuator, ActuatorSupervisor classes. Add Status enum. Turn all into Python modules.
    Signed-off-by: Szymon Duchniewicz <szduchniewicz@gmail.com>

---
+++ src/Utils/status.py
@@ -0,0 +1,8 @@
+import Enum
+
+class Status(Enum):
+    """Status enum defines possible status values for
+    Sensor and Actuator class objects."""
+
+    ENABLED = "enabled"
+    DISABLED = "disabled"
