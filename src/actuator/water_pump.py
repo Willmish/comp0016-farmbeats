999f63a2b48a14afe40bcc5629b1c2404b139de8
mithrandir_chen@hotmail.com <mithrandir_chen@hotmail.com>
2022-01-26 23:52:17 +0000

    Add skeleton code for Actuator, ActuatorSupervisor classes. Add Status enum. Turn all into Python modules.
    Signed-off-by: Szymon Duchniewicz <szduchniewicz@gmail.com>
    
    sensor skeleton added
    
    sensor skeleton added
    
    pid template added
    
    Add comment
    
    simple testing added
    
    humidity controller
    
    update clockspeed
    
    update
    
    update
    
    Cleanup old files.
    
    Signed-off-by: Szymon Duchniewicz <szduchniewicz@gmail.com>
    
    Fix pep8 violations. (Format with python black).
    
    Signed-off-by: Szymon Duchniewicz <szduchniewicz@gmail.com>
    
    Update pid.py
    
    Update humidity_pid.py
    
    Update test_pid.py
    
    modified actuator, move test file into seperate directory
    
    incoorperate pid into humidity
    
    add water_pump tp actuator, water_analyser to analyser
    
    renew
    
    mistake fix
    
    water_level_sensor v1
    
    sensor update

---
+++ src/actuator/water_pump.py
@@ -0,0 +1,44 @@
+from tools.status import Status
+from actuator.actuator import Actuator
+from pubsub import pub
+import RPi.GPIO as GPIO
+
+# Actuator Interface
+
+
+class Waterpump(Actuator):
+    PUMP_PIN = 10
+
+    def __init__(self, *args, **kwargs):
+
+        """__init__ Initialise an Actuator Interface.
+        :param actuator_type:
+        :type actuator_type: str
+        :param actuator_id:
+        :type actuator_id: int
+        :param actuator_status:
+        :type actuator_status: Status
+        """
+        super().__init__("water_pump", args, kwargs)
+        self._on = 0
+        pub.subscribe(self.water_pump_listener, "actuator.pump_status")
+        GPIO.setup(Waterpump.PUMP_PIN, GPIO.OUT)
+
+    def activate(self):
+        """activate: sets the current status to Status.ENABLED."""
+        self._status = Status.ENABLED
+
+    def actuate(self):
+        """actuate: dummy actuation function, to be overriden by children."""
+        # Turns LED on and off
+        if self._on > 0:
+            print("pump on!")
+            GPIO.output(Waterpump.LED_PIN, GPIO.HIGH)
+        else:
+            print("pump off!")
+            GPIO.output(Waterpump.LED_PIN, GPIO.LOW)
+
+    def pump_status_listener(self, args, rest=None):
+        status = args
+        print("Received pump vals over pubsub:", status)
+        self._on = status
---
+++ src/sensor/water_level.py
@@ -0,0 +1,24 @@
+from time import time
+from pubsub import pub
+from sensor.sensor import Sensor
+from tools.status import Status
+from tools.sensor_data import SensorData
+
+
+class WaterLevel(Sensor):
+    WHATER_LEVEL_PIN = 0
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(("waterlevel"), *args, **kwargs)
+
+    def collect(self):
+        self._status = Status.ENABLED
+        pub.sendMessage(
+            "sensor_data.water_level_sensor",
+            args=SensorData(
+                time(), self._id, self._type
+            ),
+        )
+
+    def disable(self):
+        self._status = Status.DISABLED
