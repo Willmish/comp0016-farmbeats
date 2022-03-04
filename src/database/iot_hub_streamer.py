cf34f882582689b4aaa1d8ff69ba0aa81c7c1798
Szymon Duchniewicz <szduchniewicz@gmail.com>
2022-03-04 13:16:09 +0000

    Add IoTHubStreamer code. Add .env.template.
    Signed-off-by: Szymon Duchniewicz <szduchniewicz@gmail.com>

---
+++ src/database/iot_hub_streamer.py
@@ -0,0 +1,46 @@
+from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
+from dotenv import load_dotenv
+from pubsub import pub
+import os
+
+
+class IoTHubStreamer:
+    sensor_data_topic = "sensor_data"
+    MSG_TEXT = '{{"Timestamp": {timestamp},\
+                "SensorID": {sensor_id},\
+                "SensorType": {sensor_type},\
+                "Value": {value}}}'
+
+    def __init__(self):
+        load_dotenv()
+        self._connection_string = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+        self._client = IoTHubDeviceClient.create_from_connection_string(self._connection_string)
+
+    def __enter__(self):
+        self._client.connect()
+        pub.subscribe(self.sensor_data_listener,
+                      IoTHubStreamer.sensor_data_topic)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._client.shutdown()
+        pub.unsubscribe(self.sensor_data_listener, IoTHubStreamer.sensor_data_topic)
+
+    def sensor_data_listener(self, args):
+        print("IoTHubStreamer: Received data over pubsub ", args)
+        msg_text_formatted = IoTHubStreamer.MSG_TEXT.format(timestamp=args.timestamp,
+                                             sensor_id=args.sensor_id,
+                                             sensor_type=args.sensor_type,
+                                             value=args.sensor_value)
+        message = Message(msg_text_formatted)
+        self._client.send_message(message)
+
+if __name__ == "__main__":
+    # Test the IoTHub connection.
+    import sys
+    from time import time
+    with IoTHubStreamer() as streamer:
+        sys.path.insert(0, '..')
+        from tools.sensor_data import SensorData
+        pub.sendMessage("sensor_data", args=SensorData(time(),
+                        -1, "test_sensor_type", -999))
