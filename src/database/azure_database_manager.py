88cc2b0e018cd74b17731771a932dbadcd7de028
Szymon Duchniewicz <szduchniewicz@gmail.com>
2022-03-04 21:09:31 +0000

    Reformat azure_db_manager to use .env file. - Remove src/database/test.py. - Reformat src/database/iot_hub_streamer.py.
    Signed-off-by: Szymon Duchniewicz <szduchniewicz@gmail.com>

--- src/database/azure_database_manager.py
+++ src/database/azure_database_manager.py
@@ -1,15 +1,8 @@
 from pubsub import pub
 from database_manager import DatabaseManager
+from dotenv import load_dotenv
+import os
 import pyodbc
-
-# copied from src to manually add data
-
-
-server = "iot-farmbeats.database.windows.net"
-database = "iot-farmbeats"
-username = "iotFarmBeats2022"
-password = "{plantGrowth22}"
-driver = "{ODBC Driver 18 for SQL Server}"
 
 
 class AzureDatabaseManager(DatabaseManager):
@@ -17,20 +10,11 @@ class AzureDatabaseManager(DatabaseManager):
 
     def __init__(self, database_path: str = "test.db"):
         super().__init__("azure_db")
+        load_dotenv()
+        self._connection_string = os.getenv("DATABASE_CONNECTION_STRING")
 
     def __enter__(self):
-        self._azure_conn = pyodbc.connect(
-            "DRIVER="
-            + driver
-            + ";SERVER=tcp:"
-            + server
-            + ";PORT=1433;DATABASE="
-            + database
-            + ";UID="
-            + username
-            + ";PWD="
-            + password
-        )
+        self._azure_conn = pyodbc.connect(self._connection_string)
         self._cursor = self._azure_conn.cursor()
         return self
 
@@ -108,33 +92,43 @@ if __name__ == "__main__":
     def get_current_time_iso_cut():
         # Get current time in ISO format, ODBC acceptable (without fractional seconds)
         date = datetime.now().isoformat()
-        return date[:date.find('.')]
+        return date[: date.find(".")]
 
     with AzureDatabaseManager() as db:
         db.create_sensor_data_table()
         print(get_current_time_iso_cut())
         pub.sendMessage(
             "sensor_data",
-            args=SensorData(get_current_time_iso_cut(), -1, "test_sensor_type", -999),
+            args=SensorData(
+                get_current_time_iso_cut(), -1, "test_sensor_type", -999
+            ),
         )
 
         val = 0
         for x in range(3):
             pub.sendMessage(
                 "sensor_data",
-                args=SensorData(get_current_time_iso_cut(), 1, "brightness", 900 + val),
+                args=SensorData(
+                    get_current_time_iso_cut(), 1, "brightness", 900 + val
+                ),
             )
             pub.sendMessage(
                 "sensor_data",
-                args=SensorData(get_current_time_iso_cut(), 2, "humidity", 55 + val)
+                args=SensorData(
+                    get_current_time_iso_cut(), 2, "humidity", 55 + val
+                ),
             )
             pub.sendMessage(
                 "sensor_data",
-                args=SensorData(get_current_time_iso_cut(), 3, "temperature", 20 + val),
+                args=SensorData(
+                    get_current_time_iso_cut(), 3, "temperature", 20 + val
+                ),
             )
             pub.sendMessage(
                 "sensor_data",
-                args=SensorData(get_current_time_iso_cut(), 4, "water level", 15 + val),
+                args=SensorData(
+                    get_current_time_iso_cut(), 4, "water level", 15 + val
+                ),
             )
             val += 1
 
