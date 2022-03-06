1d3c7fc80419ddfbb70d7e9823482a8d00f29438
mithrandir_chen@hotmail.com <mithrandir_chen@hotmail.com>
2022-03-06 20:36:04 +0000

    incoorperate pid into humidity

--- src/analyser/humidty_analyser.py
+++ src/analyser/humidty_analyser.py
@@ -1,6 +1,8 @@
+from humidity_pid import humidity_pid_control
 from pubsub import pub
 from analyser.analyser import Analyser
-
+from pid import PID
+import time
 
 class HumidityAnalyser(Analyser):
     def __init__(self, *args, **kwargs):
@@ -17,3 +19,16 @@ class HumidityAnalyser(Analyser):
             pub.sendMessage("actuator.fans_status", args=0.0)
             # FailsafeDefault value (for safety reasons,
             # keep it as 0, as per our security module :)
+
+    def pid_control(self, args, temprature, clock):
+        p = 1.2
+        i = 0.5
+        d = 0.001
+        pid = PID(p, i, d)
+        pid.SetPoint = temprature
+        while True:
+            feedback = args.sensor_value
+            pid.update(feedback)
+            output = 100 - pid.output
+            pub.sendMessage("actuator.fans_status", args=output)
+            time.sleep(clock)
