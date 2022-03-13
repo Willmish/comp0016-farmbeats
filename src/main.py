9d0e4412000a57a999326f6eb3180ebaef02f458
mithrandir_chen@hotmail.com <mithrandir_chen@hotmail.com>
2022-03-13 03:26:23 +0000

    multithread trial

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 from time import sleep
 from actuator.fans import Fans
 from actuator.led_lights import LEDLights
-from analyser.humidty_analyser import HumidityAnalyser
+from analyser.humidity_analyser import HumidityAnalyser
 from analyser.brightness_analyser import BrightnessAnalyser
 from analyser.moisture_analyser import WaterAnalyser
 from sensor.dht11 import DHT11
 from sensor.light_sensor import LightSensor
 from sensor.water_level import WaterLevel
+from analyser.humidity_pid import HumidityPidAnalyser
+from analyser.light_pid import LightPidAnalyser
+from analyser.water_pid import MoisturePidAnalyser
+from concurrent import futures
 
 # from data_streamer.database_manager import DatabaseManager
 from data_streamer.iot_hub_streamer import IoTHubStreamer
@@ -15,7 +19,7 @@ import RPi.GPIO as GPIO
 
 
 TIME_INTERVAL_BETWEEN_READINGS = 5
-
+PID_CLOCK_SPEED = 1
 
 def dummy_listener(args, rest=None):
     print("Received message over pubsub:", args.sensor_value)
@@ -33,6 +37,9 @@ if __name__ == "__main__":
         humidity_analyser = HumidityAnalyser()
         brightness_analyser = BrightnessAnalyser()
         water_analyser = WaterAnalyser()
+        humidity_pid = HumidityPidAnalyser()
+        light_pid = LightPidAnalyser()
+        water_pid = MoisturePidAnalyser()
 
         # Sensor DHT11 object
         dht11Sensor = DHT11(sensor_id=1)
@@ -41,16 +48,36 @@ if __name__ == "__main__":
         # Light Sensor object
         light_sensor = LightSensor(sensor_id=0)
 
+        # Create Threads
+        executor = futures.ThreadPoolExecutor(max_workers=2)
+
         pub.subscribe(dummy_listener, "humidity_sensor")
-        try:
-            while 1:
-                dht11Sensor.collect()
-                light_sensor.collect()
-                water_level.collect()
-                fans.actuate()
-                # lights.actuate()
-                # print(db)
-                sleep(TIME_INTERVAL_BETWEEN_READINGS)
-        except KeyboardInterrupt:
-            GPIO.cleanup()
-            fans.PWM_cleanup()
+
+        def original():
+            try:
+                while 1:
+                    dht11Sensor.collect()
+                    light_sensor.collect()
+                    water_level.collect()
+                    fans.actuate()
+                    # lights.actuate()
+                    # print(db)
+                    sleep(TIME_INTERVAL_BETWEEN_READINGS)
+            except KeyboardInterrupt:
+                GPIO.cleanup()
+                fans.PWM_cleanup()
+        
+        def pid():
+            try:
+                while 1:
+                    humidity_pid.collect()
+                    light_pid.collect()
+                    water_pid.collect()
+                    sleep(PID_CLOCK_SPEED)
+            except KeyboardInterrupt:
+                GPIO.cleanup()
+                fans.PWM_cleanup()
+
+        while True:
+            executor.submit(pid)
+            executor.submit(original)
