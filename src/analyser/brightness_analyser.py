45708013f3b60e199e202b8dbf01b551cd49b563
Szymon Duchniewicz <szymon.duchniewicz@avanade.com>
2022-03-14 16:33:24 +0000

    Update most PID analysers with new pubsub topics.
    Signed-off-by: Szymon Duchniewicz <szymon.duchniewicz@avanade.com>

--- src/analyser/brightness_analyser.py
+++ src/analyser/brightness_analyser.py
@@ -1,3 +1,4 @@
+from tkinter.tix import MAIN
 from pubsub import pub
 from analyser.analyser import Analyser
 
@@ -7,6 +8,7 @@ class BrightnessAnalyser(Analyser):
         super().__init__(["sensor_data.light_sensor"])
 
     def analyser_listener(self, args, rest=None):
+        MAIN_PUBSUB_TOPIC = "pid_update"  # TODO move to enum/config file
         print(args.sensor_value)
         sensor_data = args
         brightness = args.sensor_value
@@ -15,13 +17,16 @@ class BrightnessAnalyser(Analyser):
         if brightness > 270:
             sensor_data.actuator_value = 0.5
             pub.sendMessage(
-                "actuator.light_status", args=sensor_data
+                f"{MAIN_PUBSUB_TOPIC}.actuator.light_status", args=sensor_data
             )  # Lights 50% on
         elif brightness > 270:
             sensor_data.actuator_value = 0.2
             pub.sendMessage(
-                "actuator.light_status", args=sensor_data
+                f"{MAIN_PUBSUB_TOPIC}.actuator.light_status", args=sensor_data
             )  # Lights 20% on
         else:
             sensor_data.actuator_value = 0
-            pub.sendMessage("actuator.light_status", args=sensor_data)
+            pub.sendMessage(f"{MAIN_PUBSUB_TOPIC}.actuator.light_status", args=sensor_data)
+
+    def analyser_datastream_update_listener(self, args, rest=None):
+        pass
